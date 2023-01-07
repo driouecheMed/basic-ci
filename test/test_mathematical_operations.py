fff9d6c36141cd6c68e683285c56d2891ba7c718
driouecheMed <drioueche.med@gmail.com>
2023-01-07 16:13:30 +0100

    fix lint

--- test/test_mathematical_operations.py
+++ test/test_mathematical_operations.py
@@ -2,12 +2,12 @@ import unittest
 from src.mathematical_operations import my_sum
 from src.mathematical_operations import my_prod
 
+
 class TestMathematicalOperations(unittest.TestCase):
 
     def test_sum(self):
         self.assertEqual(my_sum(1, 2), 3)
         self.assertEqual(my_sum(1, 2, 7), 10)
-
 
     def test_prod(self):
         self.assertEqual(my_prod(1, 2), 2)
